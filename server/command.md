8a1d6c55c7b6cadd65c26f10f1d5d7ad7fff5733
Jefte Costa <jefteamorim@gmail.com>
2021-09-26 22:24:29 -0300

    feat(commet): crian~]ao do entpoint de coments

---
+++ server/command.md
@@ -0,0 +1,15 @@
+Inside that directory, you can run several commands:
+
+  npm run dev
+    Starts the development server.
+
+  npm run build
+    Builds the app for production.
+
+  npm start
+    Runs the built app in production mode.
+
+We suggest that you begin by typing:
+
+  cd comments-api
+  npm run dev
---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const app = require('express')();
+const consign = require('consign');
+
+const database = require('./src/Config/database');
+
+app.db = database;
+
+  consign()
+  .then('./src/Models')
+  //.then('./src/Config/passport')
+  .then('./src/Config/Middlewares')
+  .then('./src/Lib')
+  .then('./src/Controllers')
+  .then('./src/Schedules')
+  .into(app)
+
+  app.listen(process.env.HOST_PORT, () => {
+    console.log('sistema iniciado com sucesso na porta: ',process.env.HOST_PORT);
+    
+  })
