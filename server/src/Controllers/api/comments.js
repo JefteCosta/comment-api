acf9da28abc552704ccc727c24658fc28ec942b4
Jefte Costa <jefteamorim@gmail.com>
2021-09-28 17:41:30 -0300

    refactor(endpoint): atualizando endpoint de comentarios

--- server/src/Controllers/api/comments.js
+++ server/src/Controllers/api/comments.js
@@ -1,7 +1,7 @@
 module.exports = app =>{
 
     const save = async (req, res, next)=>{
-
+        res.json({date: new Date(), name:'jefte amorim da costa'})
     }
     const show = async (req, res, next)=>{
         res.json({date: new Date(), name:'jefte amorim da costa'})
